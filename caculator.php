2641cc4c5007ce0868226e822eddfc8f9007b86b
leoball <liyang.ucla@outlook.com>
2018-04-12 14:22:22 -0700

    1st commit

---
+++ caculator.php
@@ -0,0 +1,27 @@
+<html>
+  <head>
+    <title>My Caculator</title>
+  </head>
+<body>
+  <h1>Calculator</h1>
+
+
+  <p>
+    <form action="caculator.php"form method = "GET">
+    expression:<input type="text" name ="expr"><br>
+      <input type ="submit" value ="Calculate">
+
+    </form>
+
+
+</body>
+</html>
+<?php
+  $str = 0;
+  eval(' $str = \$_GET["expr"]\; ');
+  eval(' \$str = \"$str\"; ');
+  echo $str;
+  
+  
+
+?>
