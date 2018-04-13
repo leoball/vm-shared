50f1769035cb36aa24572332e1c5295f2ac47650
leoball <liyang.ucla@outlook.com>
2018-04-13 15:48:32 -0700

    version1.0

--- caculator.php
+++ caculator.php
@@ -8,7 +8,7 @@
 
   <p>
     <form action = "caculator.php" form method = "GET">
-      expression:<input type="text" name ="expr"><br>
+      expression:<input type="text" name ="expr"><br><br>
       <input type ="submit" value ="Calculate">
   </form>
 
@@ -17,8 +17,24 @@
 </html>
 
 <?php
-  $str = 0;
+  if(isset($_GET["expr"])){
+  $res = 0;
+  $pattern = "/^-?\d+(\.\d+)?\s*([+\-\\*]\s*\d+(\.\d+)?\s*)*$/";
+   $zeropattern = '/^-?00+(\.\d+)?\s*([+\-\\*]\s*00+(\.\d+)?\s*)*$/';
+
+  
+
   $str = $_GET["expr"];
-  eval('$str = '.$str.';');
-  echo $str;
+  if(preg_match($zeropattern, $str)){
+    echo "Invalid syntax.<br>";
+  }
+  else if(preg_match($pattern, $str)){
+  eval('$res = '.$str.';');
+  echo $str." = ".$res;
+  }
+  else{
+  	echo "Invalid syntax.<br>";
+  }
+}
+
 ?>
