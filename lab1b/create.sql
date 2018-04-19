24c8666f311c058b9f2b467bf850c73988499161
Sihan Min <sihanmin@yeah.net>
2018-04-18 17:33:22 -0700

    +Not null

--- lab1b/create.sql
+++ lab1b/create.sql
@@ -1,62 +1,68 @@
 
 CREATE TABLE Movie (
-	id				INT PRIMARY KEY,
-	title			VARCHAR(100),
-	year			INT,
-	rating			VARCHAR(10),
-	company			VARCHAR(50)
-);
-CREATE TABLE Actor(
 	id INT PRIMARY KEY,
-	last VARCHAR(20),
-	first VARCHAR(20),
-	sex VARCHAR(6),
-	dob DATE,
-	dod DATE
+	title VARCHAR(100) NOT NULL,
+	year INT NOT NULL,
+	rating VARCHAR(10),
+	company VARCHAR(50)
+) ENGINE = INNODB;
 
-);
-CREATE TABLE Director(
+CREATE TABLE Actor (
 	id INT PRIMARY KEY,
-	last VARCHAR(20),
-	first VARCHAR(20),
-	dob DATE,
+	last VARCHAR(20) NOT NULL,
+	first VARCHAR(20) NOT NULL,
+	sex VARCHAR(6) NOT NULL,
+	dob DATE NOT NULL,
 	dod DATE
+) ENGINE = INNODB;
 
-
-);
+CREATE TABLE Director (
+	id INT PRIMARY KEY,
+	last VARCHAR(20) NOT NULL,
+	first VARCHAR(20) NOT NULL,
+	dob DATE NOT NULL,
+	dod DATE
+) ENGINE = INNODB;
 
 CREATE TABLE MovieGenre (
-	mid				INT PRIMARY KEY,
-	genre			VARCHAR(20)
-);
-CREATE TABLE MovieDirector(
+	mid INT PRIMARY KEY NOT NULL,
+	genre VARCHAR(20) NOT NULL,
+	Foreign KEY (mid) REFERENCES Movie(id)
+) ENGINE = INNODB;
+
+CREATE TABLE MovieDirector (
 	mid INT PRIMARY KEY,
-	did INT
-) 
+	did INT NOT NULL,
+	Foreign KEY (mid) REFERENCES Movie(id)
+	Foreign KEY (did) REFERENCES Director(id)
+) ENGINE = INNODB;
 
 CREATE TABLE MovieActor (
-	mid				INT,
-	aid				INT,
-	role			VARCHAR(50),
+	mid INT,
+	aid INT,
+	role VARCHAR(50),
 	PRIMARY KEY(mid, aid)
-);
+	Foreign KEY (mid) REFERENCES Movie(id)
+	Foreign KEY (aid) REFERENCES Actor(id)
+) ENGINE = INNODB;
 
 CREATE TABLE Review (
-	name			VARCHAR(20),
-	time			TIMESTAMP,
-	mid				INT,
-	rating			INT,
-	comment			VARCHAR(500),
+	name VARCHAR(20),
+	time TIMESTAMP,
+	mid INT NOT NULL,
+	rating INT NOT NULL,
+	comment VARCHAR(500),
 	PRIMARY KEY(name, time)
-);
+	Foreign KEY (mid) REFERENCES Movie(id)
+) ENGINE = INNODB;
 
 CREATE TABLE MaxPersonID (
-	id				INT PRIMARY KEY
-);
+	id INT PRIMARY KEY
+) ENGINE = INNODB;
 
 CREATE TABLE MaxMovieID(id) (
-	id				INT PRIMARY KEY
-);
+	id INT PRIMARY KEY
+) ENGINE = INNODB;
 
 
 
