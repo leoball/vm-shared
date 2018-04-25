4b2fac45ff594ac8f48ef13265e386adf7754b9c
leoball <liyang.ucla@outlook.com>
2018-04-25 15:45:51 -0700

    first

---
+++ lab1c/index.php
@@ -0,0 +1,44 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <title>CS143 Project 1c</title>
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <link href="./bootstrap.css" rel="stylesheet">
+</head>
+  
+<body>
+  <div class="container">
+      <div class="row">
+        <div class="col-sm-3 col-md-2 col-mg-10 sidebar">
+          <ul class="nav nav-sidebar">
+            <p>INPUT_PAGE</p>
+            <li><a href="Add_AD.php">Add Actor/Director</a></li>
+            <li><a href="Add_M.php">Add Movie Information</a></li>
+            <li><a href="Add_MAR.php">Add Movie/Actor Relation</a></li>
+            <li><a href="Add_MDR.php">Add Movie/Director Relation</a></li>
+          </ul>
+          
+          <ul class="nav nav-sidebar">
+            <p>BROWSING_PAGE: </p>
+            <li><a href="Show_A.php">Show Actor Information</a></li>
+            <li><a href="Show_M.php">Show Movie Information</a></li>
+          </ul>
+          
+          <ul class="nav nav-sidebar">
+            <p>SEARCH_PAGE:</p>
+            <li><a href="search.php">Search/Actor Movie</a></li>
+          </ul>
+        </div>
+        <div class="col-sm-9 col-sm-offset-3 col-md-10 col-md-offset-2 main">
+             <div class="jumbotron">
+                
+             </div>
+
+        </div>
+      </div>
+    </div>
+  
+
+</body>
+</html>
