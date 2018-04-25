c54d16fd539fb39a037ba2743d695053b564178f
leoball <liyang.ucla@outlook.com>
2018-04-25 16:11:13 -0700

    v1.1

--- lab1c/index.php
+++ lab1c/index.php
@@ -1,39 +1,61 @@
+
 <!DOCTYPE html>
 <html lang="en">
-<head>
-  <title>CS143 Project 1c</title>
-  <meta charset="utf-8">
-  <meta name="viewport" content="width=device-width, initial-scale=1">
-  <link href="./bootstrap.css" rel="stylesheet">
-</head>
-  
-<body>
-  <div class="container">
+  <head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <!-- The above 3 meta tags *must* come first in the head; any other head content must come *after* these tags -->
+    <title>CS143 Project 1c</title>
+
+    <!-- Bootstrap -->
+    <link href="bootstrap.min.css" rel="stylesheet">
+    <link href="lab1c.css" rel="stylesheet">
+
+  <body>
+
+    <nav class="navbar navbar-inverse navbar-fixed-top">
+      <div class="container-fluid">
+        <div class="navbar-header navbar-defalt">
+          <a class="navbar-brand" href="index.php">CS143 DataBase Query System (Demo)</a>
+        </div>
+      </div>
+    </nav>
+
+    <div class="container">
       <div class="row">
-        <div class="col-sm-3 col-md-2 col-mg-10 sidebar">
+        <div class="col-sm-3 col-md-2 sidebar">
           <ul class="nav nav-sidebar">
-            <p>INPUT_PAGE</p>
-            <li><a href="Add_AD.php">Add Actor/Director</a></li>
-            <li><a href="Add_M.php">Add Movie Information</a></li>
-            <li><a href="Add_MAR.php">Add Movie/Actor Relation</a></li>
-            <li><a href="Add_MDR.php">Add Movie/Director Relation</a></li>
+            <p>&nbsp;&nbsp;Add new content</p>
+            <li><a href="homepage.php">Add Actor/Director</a></li>
+            <li><a href="Add_movie.php">Add Movie Information</a></li>
+            <li><a href="Add_M_A_R.php">Add Movie/Actor Relation</a></li>
+            <li><a href="Add_M_D_R.php">Add Movie/Director Relation</a></li>
           </ul>
-          
           <ul class="nav nav-sidebar">
-            <p>BROWSING_PAGE: </p>
+            <p>&nbsp;&nbsp;Browsering Content :</p>
             <li><a href="Show_A.php">Show Actor Information</a></li>
             <li><a href="Show_M.php">Show Movie Information</a></li>
           </ul>
-          
           <ul class="nav nav-sidebar">
-            <p>SEARCH_PAGE:</p>
+            <p>&nbsp;&nbsp;Search Interface:</p>
             <li><a href="search.php">Search/Actor Movie</a></li>
           </ul>
         </div>
         <div class="col-sm-9 col-sm-offset-3 col-md-10 col-md-offset-2 main">
              <div class="jumbotron">
-                
-             </div>
+                <h2>Welcome to CS143 Query System! (Demo)</h2>
+                <br>
+    Declariation:
+  <br><br>
+    <ul>
+      <li>This is just a simple demo site created by students Yi Wang and Zhen Feng. Thanks for their work!</li>
+      <li>This demo site may not "fully satisfy" 
+      all the requirments assigned in the spec. The only purpose of this site is to give
+      you a sense. Whenever there is a conflict, the official project spec always prevails.</li>
+      <li>Copycat is strongly discouraged. Feel free to beat this demo site.</li>
+    </ul>
+            </div>
 
         </div>
       </div>
