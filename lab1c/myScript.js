0370778d9cc80db7c5565347aba16173b6d51814
Sihan Min <sihanmin@yeah.net>
2018-04-26 14:12:04 -0700

    auto

--- lab1c/myScript.js
+++ lab1c/myScript.js
@@ -1,7 +1,8 @@
 $(document).ready(function(){
 	$("#suggest").keyup(function(){
-		$.get("Suggest_M.php", {}, function(data){
-			
+		$.get("Suggest_M.php", {movie: $(this).val()}, function(data){
+			$("datalist").empty();
+			$("datalist").html(data);
 		});
 	});
 });
